850afe1a2e91f11fca248e01833c58799c4cf894
Ahmed Elbehary <29628969+elbehary01@users.noreply.github.com>
2022-09-28 22:14:42 +0200

    create contact from.... JS Will come later

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello World!")
+
+let inputs = document.querySelectorAll("input");
+
+for(let i = 0; i< inputs.length; i++) {
+console.log(inputs[i])
+}
+// .addEventListener("input", x => console.log(x.target.value))
