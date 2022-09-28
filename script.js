264b4c1acb381dabb506714deb360854ceabff2a
Ahmed Elbehary <29628969+elbehary01@users.noreply.github.com>
2022-09-28 15:21:11 +0200

    prototype code for back ups....

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello World!")
+// console.log("Hello World!")
+// let hello = 'hello';
+// let helloo = {};
+// console.log(hello);
+// console.log(document);
+// console.log(window);
+
+// let hey = () => {
+//   console.log(hello);
+// };
+// hey();
