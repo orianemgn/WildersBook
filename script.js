5625f8ca3414c04bfc6e6282964546d4781fb747
Ahmed Elbehary <29628969+elbehary01@users.noreply.github.com>
2022-09-29 16:26:27 +0200

    work on contact form logic

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 console.log("Hello World!")
 
-let inputs = document.querySelectorAll("input");
 
-for(let i = 0; i< inputs.length; i++) {
-console.log(inputs[i])
+
+let inputs = document.querySelectorAll('input');
+
+for (let i = 0; i < inputs.length; i++) {
+  console.log(inputs[i]);
+  inputs[i].addEventListener(
+    'input',
+    function () {
+      getValue(inputs[i]);
+    },
+    false
+  );
 }
-// .addEventListener("input", x => console.log(x.target.value))
+
+const getValue = ele => {
+  let attr = ele.getAttribute('id');
+  let value = ele.value;
+  console.log(value);
+  switch (attr) {
+    case 'name':
+      checkValdity(value);
+      handleValue(value);
+      break;
+    case 'email':
+      handleValue(value);
+      break;
+    case 'telephone':
+      handleValue(value);
+      break;
+    default:
+      console.log('not here');
+  }
+};
+
+const handleValue = val => {
+  console.log(val);
+};
+
+const checkValdity = value => {
+  let smallLetters = value.toLowerCase();
+  console.log('++++++>>>>>', smallLetters);
+  let regex =
+    /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+  let found = regex.test(smallLetters);
+  console.log('========>>>>', found);
+};
