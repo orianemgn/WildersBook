933547041bc69860c6a1d5c70c7eb8226690b726
Ahmed Elbehary <ahmedelbehary0@gmail.com>
2022-09-29 17:36:55 +0200

    fix errors

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 console.log("Hello World!")
 
-//Section 3 - Wilders - Code for the modal 
-// Get the modal 
-let modal = document.getElementById("modal-box"); 
-console.log(modal.innerHTML); 
+//Section 3 - Wilders - Code for the modal
+// Get the modal
+let modal = document.getElementById("modal-box");
+// console.log(modal.innerHTML);
 
 //Get the button that opens the modal
-let btn = document.getElementById("modal-button"); 
+let btn = document.getElementById('modal-button');
 
-//Get the span element that closes the modal 
+//Get the span element that closes the modal
 let span = document.getElementsByClassName("close")[0];
 console.log("SPAN", span)
 
 
 // When the user clicks on the button, open the modal
-btn.onclick = function(){
-    modal.style.display="block"; 
-}; 
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
 
 //When the user clicks on span (x), close the modal
-span.onclick = function(){
-    modal.style.display = "none"; 
-}; 
+span.onclick = function () {
+  modal.style.display = 'none';
+};
 
 //When the user clicks anywhere outsite the modal, close interface
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }; 
+window.onclick = function (event) {
+  if (event.target == modal) {
+    // modal.style.display = 'none';
+  }
+};
 
 
 //Section 4 - Contact
@@ -77,7 +77,6 @@ const checkValdity = value => {
   let found = regex.test(smallLetters);
   console.log('========>>>>', found);
 };
-=======
 
 // console.log("Hello World!")
 // let hello = 'hello';
@@ -91,6 +90,4 @@ const checkValdity = value => {
 // };
 // hey();
 
-console.log("Hello World!")
-
-
+console.log('Hello World!');
