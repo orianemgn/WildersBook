0422c061820ac9b48833ae6cc820af8d438fdf64
orianemgn <mgnoriane@gmail.com>
2022-09-30 09:44:04 +0200

    changed color design

--- script.js
+++ script.js
@@ -78,27 +78,6 @@ window.onclick = function(event) {
 
 
 
-
-
-
-// When the user clicks on the button, open the modal
-//btn.onclick = function(){
-//    modal.style.display="block"; 
-//}; 
-
-//When the user clicks on span (x), close the modal
-//span.onclick = function(){
-//    modal.style.display = "none"; 
-//}; 
-
-//When the user clicks anywhere outsite the modal, close interface
-// window.onclick = function(event) {
-    // if (event.target == modal) {
-      // modal.style.display = "none";
-    // }
-  // }; 
-
-
 //Section 4 - Contact
 let inputs = document.querySelectorAll('input');
 
