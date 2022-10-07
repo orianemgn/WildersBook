c84b85b006d91ea7abf70e2f4415d2ce58991ea3
orianemgn <mgnoriane@gmail.com>
2022-10-07 15:51:43 +0200

    correction name stefan

--- script.js
+++ script.js
@@ -124,7 +124,7 @@ let wildersGroup2 = [
   },
   {
     id: 14,
-    name: "Stedan D", 
+    name: "Stefan D", 
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     picture: "assests/7848.jpg", 
     linkedin: "https://www.linkedin.com/in/magninoriane/", 
